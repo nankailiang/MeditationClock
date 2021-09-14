12e343ac79c1469fe7a69f45a360423fa7ce782e
wangShuangXuan <3034562384@qq.com>
2021-09-14 20:57:48 +0800

    更新日志

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,10 +1,11 @@
-var _app = getApp()
+var app = getApp()
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    userinfo: {},
+    userInfo:null,
+    isShowUserName:false,
     openid: "",
     name: "",
     menuitems: [
@@ -13,44 +14,44 @@ Page({
       { text: '关于作者', url: '../author/author', icon: '/images/user4.png', arrows: '/images/arrows.png' }
     ]
   },
-  onGotUserInfo: function (e) {
-    const that = this;
-    wx.cloud.callFunction({
-      name: "login",
-      success: res => {
-        console.log("云函数调用成功")
-        that.setData({
-          openid: res.result.openid,
-          userinfo: e.detail.userInfo,
-          name: e.detail.userInfo.nickName
+  getUserProfile(){
+    wx.getUserProfile({
+      desc: '用于完善用户资料',
+      success:(res)=>{
+        console.log("获取用户信息成功！",res);
+        let user = res.userInfo
+        wx.setStorageSync('user', user) //保存信息到本地
+        this.setData({
+          isShowUserName:true,
+          userInfo:user
         })
-        that.data.userinfo.openid = that.data.openid
-        wx.setStorageSync("userinfo", that.data.userinfo)
       },
-      fail: res => {
-        console.log("云函数调用失败")
+      fail:res=>{
+        console.log("获取用户信息失败！",res);
       }
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    const ui = wx.getStorageSync('userinfo')
+    });
+},
+  
+onShow(options){
+  wx.setNavigationBarTitle({
+    title: '我的'
+  })
+  this.getUserProfile()
+  var user = wx.getStorageSync('user')  //本地缓存取用户信息
+  if(user&&user.nickName){  //如果本地缓存有信息，显示本地缓存
     this.setData({
-      userinfo: ui,
-      openid: ui.openid,
-      name: ui.nickName
+      isShowUserName:true,
+      userInfo:user
     })
-  },
+  }
+},
 
   empty: function (e) {
-    var that = this;
     var index = e.currentTarget.dataset.index;
     if (index == 1) {
-      const that = this;
-      const ui = wx.getStorageSync('userinfo')
-      if (!ui.openid) {
+      // const ui = wx.getStorageSync('userinfo')
+      var user = wx.getStorageSync('user')
+      if (!user.nickName) {
         wx.showModal({
           title: '温馨提示',
           content: '此功能需要登录后使用',
@@ -71,7 +72,7 @@ Page({
           content: '记录删除后无法找回，确定删除吗？',
           success: function (res) {
             if (res.confirm) {
-              var openid = that.data.openid;
+              var openid = app.globalData.openid;
               //云函数删除
               wx.cloud.callFunction({
                 name: "deletelog",
